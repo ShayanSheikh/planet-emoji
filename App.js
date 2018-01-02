0eecd69e5fce387eada8e25650fed5c0083d0290
Shayan Sheikh <najam.shayan@gmail.com>
2018-01-02 12:13:03 -0500

    Building out components

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Home from './components/Home';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>WHY GOD</Text>
+        <Text>WHY HAST THOU FORSAKEN ME</Text>
+        <Text>probably all the blasphemy</Text>
       </View>
     );
   }
 }
+
+const RootNavigator = StackNavigator({
+  Main: {
+    screen: Home,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
