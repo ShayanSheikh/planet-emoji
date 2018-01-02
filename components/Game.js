c5e10f35561dee98be0c644f4f329cb12bc87440
ohagert1 <ohagerty@gmail.com>
2018-01-02 17:07:09 -0500

    some styling, still sucks

--- components/Game.js
+++ components/Game.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { emojis } from '../utils/emojis';
+const image = require('../assets/globe.png');
 var _ = require('lodash');
 
 export default class Game extends React.Component {
@@ -12,32 +13,46 @@ export default class Game extends React.Component {
       curr: {},
       score: 0,
       guess: '',
-      message: ""
+      message: "Make a guess!",
+      active: true
     }
     this.pickRandomQuestion = this.pickRandomQuestion.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.restartGame = this.restartGame.bind(this);
+    this.advanceGame = this.advanceGame.bind(this);
+  }
+
+  advanceGame() {
+    let ind = this.pickRandomQuestion(this.state.emojis);
+    let active = (this.state.emojis.length !== 0);
+    if(active) {
+      this.setState({ curr: this.state.emojis[ind], emojis: this.state.emojis.slice(0, ind).concat(this.state.emojis.slice(ind+1))});
+    } else {
+      this.setState({ active })
+    }
+
   }
 
   restartGame() {
-    let ind = this.pickRandomQuestion(this.state.emojis);
-    this.setState({ curr: this.state.emojis[ind], emojis: this.state.emojis.slice(0, ind).concat(this.state.emojis.slice(ind+1)) });
-  };
+    let ind = this.pickRandomQuestion(emojis);
+    let curr = emojis[ind];
+    console.log(curr)
+    this.setState({ curr, emojis: emojis.slice(0, ind).concat(emojis.slice(ind+1)), score: 0, message: "Make a guess!", active: true});
+  }
 
   pickRandomQuestion(arr) {
-    return Math.floor(Math.random() * arr.length);
-  };
+    return Math.floor(Math.random() * (arr.length - 1));
+  }
 
   componentDidMount() {
     this.restartGame();
-  };
+  }
 
   handleSubmit() {
     let guess = this.state.guess.replace(/\W+/g, " ").toLowerCase();
     let result = (guess === this.state.curr.answer);
-    this.restartGame();
-
-    this.setState({ guess: '', score: (result) ? this.state.score+10 : this.state.score })
+    if(result) this.advanceGame();
+    this.setState({ guess: '', score: (result) ? this.state.score+10 : this.state.score, message: (result) ? "Correct! Next question..." : "Nope, you're terrible at this." });
   }
 
   handleChange(guess) {
@@ -45,29 +60,61 @@ export default class Game extends React.Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          {
-            this.state.emojis.length && this.state.curr.question
-          }
-        </Text>
-        <Text> Score: { this.state.score } </Text>
-        <TextInput
-          onChangeText={(guess) => this.handleChange(guess)}
-          value={this.state.guess}
-          placeholder="Guess"
-        />
-        <Button
-          onPress = {this.handleSubmit}
-          title = "Submit Guess"
-        />
-        <Button
-          onPress = {this.restartGame}
-          title = "Give up like a lil bitch"
-        />
-      </View>
-    )
+    if(this.state.active) {
+      return (
+          <View style={styles.container}>
+            <Image source={image} style={styles.image}/>
+            <View style={styles.centerer}>
+              <View style={styles.gameInfo}>
+                <Text style={{textAlign: 'center'}}>
+                  {this.state.message}
+                </Text>
+                  <Text style={{textAlign: 'center'}}>
+                    {
+                      this.state.curr.question
+                    }
+                  </Text>
+                  <Text style={{textAlign: 'center'}}> Score: { this.state.score } </Text>
+                </View>
+                <View style={styles.gamePlay}>
+                  <TextInput
+                    style={{textAlign: 'center'}}
+                    onChangeText={(guess) => this.handleChange(guess)}
+                    value={this.state.guess}
+                    placeholder="Guess"
+                  />
+                  <Button
+                    color="green"
+                    onPress = {this.handleSubmit}
+                    title = "Submit Guess"
+                  />
+                </View>
+                <View style={styles.gameManager}>
+                  <Button
+                    color="red"
+                    onPress = {this.restartGame}
+                    title = "Give up like a lil bitch"
+                  />
+                </View>
+            </View>
+          </View>
+      )
+    } else {
+        return (
+          <View>
+            <View style={styles.gameInfo}>
+              <Text style={{textAlign: 'center'}}>
+                You Win!
+              </Text>
+              <Button
+                onPress = {this.restartGame}
+                title = "Restart"
+              />
+            </View>
+          </View>
+        )
+      }
+
   }
 };
 
@@ -75,7 +122,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#AAA',
+    backgroundColor: 'rgba(185, 185, 185, .5)',
     justifyContent: 'center'
+  },
+  gameInfo: {
+    margin: 5,
+    backgroundColor: 'rgba(185, 185, 185, .75)'
+  },
+  gamePlay:{
+    margin: 5,
+    backgroundColor: 'rgba(185, 185, 185, .75)'
+  },
+  gameManager: {
+    margin: 5,
+    backgroundColor: 'rgba(185, 185, 185, .75)'
+  },
+  image: {
+    zIndex: -10,
+    flex: 1,
+    alignSelf: 'stretch',
+    width: null,
+  },
+  centerer: {
+    bottom: 50 + "%"
   }
 })
--- utils/emojis.js
+++ utils/emojis.js
@@ -3,11 +3,12 @@ export const emojis = [
   { question: '👨🔛🌙', answer: 'man on the moon' },
   { question: '☔️🐱🐶', answer: 'raining cats and dogs' },
   { question: '🥊🍺❤️', answer: 'punch drunk love' },
-  { question: '🚫😭⤴️⤵️💦🥛', answer: 'don\'t cry over spilled milk' },
+  { question: '🚫😭⤴️⤵️💦🥛', answer: 'dont cry over spilled milk' },
   { question: '🙏💩', answer: 'holy crap' },
   { question: '👀🐯', answer: 'eye of the tiger' },
-  { question: '🐝🐝↔️🚫🐝🐝➡️❓', answer: 'to be or not to be, that is the question' },
-  { question: '🚫☠️🔪➡️💪', answer: 'what doesn\'t kill you makes you stronger' },
+  { question: '🐝🐝↔️🚫🐝🐝➡️❓', answer: 'to be or not to be that is the question' },
+  { question: '🚫☠️🔪➡️💪', answer: 'what doesnt kill you makes you stronger' },
   { question: '🚫🤡🔁', answer: 'stop clowning around' },
   { question: '🐦✋🐦🐦🌳', answer: 'a bird in the hand is worth two in the bush' }
 ]
+
